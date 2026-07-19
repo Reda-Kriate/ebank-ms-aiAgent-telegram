ccc579eb18417469ecd4f24199b7c1e3e31bd94b
Reda-Kriate <kriatereda@gmail.com>
2026-07-19 19:12:12 +0100

    commit

---
+++ src/main/java/org/reda/ebankmstelegram/EbankMsTelegramApplication.java
@@ -0,0 +1,13 @@
+package org.reda.ebankmstelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankMsTelegramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankMsTelegramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/reda/ebankmstelegram/EbankMsTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package org.reda.ebankmstelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankMsTelegramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
